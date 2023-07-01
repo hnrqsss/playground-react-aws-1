5b2ab9e47bf7aef7935a50c285087f1fe487876c
Cicero Viana <cicerohen@gmail.com>
2023-07-01 10:41:55 -0300

    chore

---
+++ frontend/src/types.ts
@@ -0,0 +1,16 @@
+export type Cat = {
+  id: string;
+  name: string;
+  age: string;
+  breed: string;
+};
+
+export type Breed = {
+  id: number;
+  name: string;
+};
+
+export type Age = {
+  id: number;
+  name: string;
+};
